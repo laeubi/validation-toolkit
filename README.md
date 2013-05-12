91c3294b64c7c255862a68d629d66b02e604f862
laeubi <laeubi@laeubi-soft.de>
2013-05-12 14:39:15 +0200

    First Version that supports W3C markup validator

--- README.md
+++ README.md
@@ -2,3 +2,83 @@ validation-toolkit
 ==================
 
 Providing a toolkit to automate common validation tasks for web-documents, especially the W3C toolkit (markup, css, ...)
+
+The aim is to allow automated testing of single (X)HTML and CSS documents, or even a whole page against common validation services e.g. the W3C Markup Validation service.
+The current version includes an ANT task to check a document and either fail or just print out all problems, this can then be used in Integration Servers like Hudson/Jenkins to run auotmated tests.
+
+features
+--------
+- [x] W3C MarkupValidator Task
+- [x] Hudson/Jenkins parser for generated Markup-Validator errors
+- [x] recursively check HTML document using public URLs
+- [ ] W3C CSS Validation Task
+- [ ] Hudson/Jenkins parser for generated CSS-Validator errors
+- [ ] JS Linting/Checking task
+- [ ] Hudson/Jenkins parser for generated JS Linting/Checking errors
+- [ ] Full-(recursive)Page checker that validates/checks HTML, CSS and JS
+- [ ] ... ideas are welcome ...
+
+usage
+-----
+Fist download the latest build from the 'assembly' directory (or build your own version, see below).
+Extract all files and define a new task in your build.xml
+```xml
+<path id="classpath.validator" >
+	<fileset dir="/path/to/files">
+		<include name="*.jar" />
+	</fileset>
+</path>
+<taskdef classname="de.laeubisoft.tools.ant.validation.W3CMarkupValidationTask" classpathref="classpath.markupvalidator" name="W3CMarkupValidation"></taskdef>
+```
+Now you can use the new task like this:
+```xml
+<W3CMarkupValidation uri="http://example.test" />
+```
+This will check the page against the online version of the validator (http://validator.w3.org/), alternatively you can validate a fragment
+```xml
+<W3CMarkupValidation fragment="${propertyHoldingHTMLFragmentToValidate}" />
+```
+or even use file upload
+```xml
+<W3CMarkupValidation file="/file/to/send.html" />
+```
+It is recommend to install a private copy of the validator (see http://validator.w3.org/docs/install.html) for maximum performance, you can specify an alternative URL (this of course also applies to fragments and file uploads) like this:
+```xml
+<W3CMarkupValidation uri="http://example.test" validator="http://localhost/w3c-markup-validator/check" />
+```
+If required you can specify charset and/or doctype via the charset and doctype attribute as well as debug option
+```xml
+<W3CMarkupValidation charset="UTF-8" doctype="XHTML1.1" uri="http://example.test" validator="http://localhost/w3c-markup-validator/check" debug="true" />
+```
+With the fail attribute, you can control if the build should be failed (fail="true") or only a message should be printed out on validation errors:
+```xml
+<W3CMarkupValidation fail="false" uri="http://example.test" validator="http://localhost/w3c-markup-validator/check" />
+```
+If you want to check all pages of a page (connected via links) you can specify the recurse attribute (this is currently only supported for URIs!), use the embedded ignore element to skip pages, links matching any of the ignore pattern are not checked.
+```xml
+<W3CMarkupValidation recurse="true" uri="http://example.test" validator="http://localhost/w3c-markup-validator/check">
+	<ignore>.*bad_page\.html</ignore>
+	<ignore>.*\.zip</ignore>
+</W3CMarkupValidation>
+```
+
+
+licence
+-------
+The code and its dependencies are distributed under 'The Apache Software License, Version 2.0' see licenses.xml for details.
+
+building
+--------
+The code is build with maven 3, you can use mvn eclipse:eclipse task to generate an Eclipse Project, and mvn install to build from commandline, since the code is very simple at the moment you can of course choose to build it with the tool you prefer as long as all dependencies are on the classpath (e.g Ivy can resolve and download maven dependencies too).
+
+dependencies
+------------
+- ant-1.8.0.jar (it should build with previous versions of ant as well since it does not use any special features)
+- ant-launcher-1.8.0.jar
+- tagsoup-1.2.jar (used for parsing html documents and extract links)
+- commons-httpclient-3.1.jar (used to communicate with the W3C API)
+- commons-logging-1.0.4.jar
+- commons-codec-1.2.jar
+
+
+
---
+++ source/src/main/java/de/laeubisoft/tools/ant/validation/W3CMarkupValidationTask.java
@@ -0,0 +1,484 @@
+/*
+ * #%L
+ * Ant Validation Toolkit
+ * %%
+ * Copyright (C) 2013 Christoph Läubrich
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package de.laeubisoft.tools.ant.validation;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethodBase;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.methods.multipart.StringPart;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
+import org.ccil.cowan.tagsoup.Parser;
+import org.w3.markup.validator.Culprit;
+import org.w3.markup.validator.Debug;
+import org.w3.markup.validator.Envelope;
+import org.w3.markup.validator.Error;
+import org.w3.markup.validator.MarkupValidationResponse;
+import org.w3.markup.validator.ObjectFactory;
+import org.w3.markup.validator.ValidationErrors;
+import org.w3.markup.validator.ValidationWarnings;
+import org.w3.markup.validator.Warning;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Provides a task for automatic checking of HTML documents
+ * 
+ * @author Christoph Läubrich
+ */
+public class W3CMarkupValidationTask extends Task {
+
+    /**
+     * 
+     */
+    private static final String W3_ORG_VALIDATOR        = "http://validator.w3.org/check";
+
+    /**
+     * We want soap !
+     */
+    private static final String VALIDATOR_FORMAT_OUTPUT = "soap12";
+
+    /**
+     * The URL of the document to validate,either this parameter, or
+     * uploaded_file, or fragment must be given.
+     */
+    private URL                 uri;
+    /**
+     * The document to validate, either this parameter, or uri, or fragment must
+     * be given.
+     */
+    private File                uploaded_file;
+    /**
+     * The source of the document to validate. Full documents only, either this
+     * parameter, or uri, or uploaded_file must be given.
+     */
+    private String              fragment;
+
+    private String              validator               = W3_ORG_VALIDATOR;
+
+    /**
+     * Character encoding override: Specify the character encoding to use when
+     * parsing the document. When used with the auxiliary parameter fbc set to
+     * 1, the given encoding will only be used as a fallback value, in case the
+     * charset is absent or unrecognized. Note that this parameter is ignored if
+     * validating a fragment with the direct input interface, by default the
+     * validator detects the charset of the document automatically.
+     */
+    private String              charset;
+    /**
+     * Document Type override: Specify the Document Type (DOCTYPE) to use when
+     * parsing the document. When used with the auxiliary parameter fbd set to
+     * 1, the given document type will only be used as a fallback value, in case
+     * the document's DOCTYPE declaration is missing or unrecognized,by default
+     * the validator detects the document type of the document automatically.
+     */
+    private String              doctype;
+    /**
+     * When set to 1, will output some extra debugging information on the
+     * validated resource (such as HTTP headers) and validation process (such as
+     * parser used, parse mode etc.). In the SOAP output, this information will
+     * be given in <m:debug> elements.
+     */
+    private boolean             debug;
+
+    /**
+     * Try to recurse into links
+     */
+    private boolean             recurse;
+    /**
+     * Should the build fail on error
+     */
+    private boolean             fail                    = true;
+
+    /**
+     * The Pattern used to format error response, see
+     * http://docs.oracle.com/javase/6/docs/api/java/util/Formatter.html#syntax
+     * for syntax
+     */
+    private String              errorPattern            = "[ERROR] [%7$s] Line %1$s, Column %2$s: %3$s (ID %4$s) source = '%5$s', %6$s";
+
+    private String              warningPattern          = "[WARNING] [%7$s] Line %1$s, Column %2$s: %3$s (ID %4$s) source = '%5$s', %6$s";
+
+    private String              debugPattern            = "[DEBUG] [%1$s] %2$s: %3$s";
+
+    private final List<Pattern> ignorePatternList       = new ArrayList<Pattern>();
+
+    public void addConfiguredIgnore(IgnorePattern ignorePattern) {
+        ignorePatternList.add(ignorePattern.toPattern());
+    }
+
+    /**
+     * @param uri
+     *            the new value for uri
+     */
+    public void setUri(URL uri) {
+        this.uri = uri;
+    }
+
+    /**
+     * @param warningPattern
+     *            the new value for warningPattern
+     */
+    public void setWarningPattern(String warningPattern) {
+        this.warningPattern = warningPattern;
+    }
+
+    /**
+     * @param debugPattern
+     *            the new value for debugPattern
+     */
+    public void setDebugPattern(String debugPattern) {
+        this.debugPattern = debugPattern;
+    }
+
+    /**
+     * @param validator
+     *            the new value for validator
+     */
+    public void setValidator(String validator) {
+        this.validator = validator;
+    }
+
+    /**
+     * @param errorPattern
+     *            the new value for errorPattern
+     */
+    public void setErrorPattern(String errorPattern) {
+        this.errorPattern = errorPattern;
+    }
+
+    /**
+     * @param fail
+     *            the new value for fail
+     */
+    public void setFail(boolean fail) {
+        this.fail = fail;
+    }
+
+    /**
+     * @param uploaded_file
+     *            the new value for uploaded_file
+     */
+    public void setFile(File uploaded_file) {
+        this.uploaded_file = uploaded_file;
+    }
+
+    /**
+     * @param recurse
+     *            the new value for recurse
+     */
+    public void setRecurse(boolean recurse) {
+        this.recurse = recurse;
+    }
+
+    /**
+     * @param fragment
+     *            the new value for fragment
+     */
+    public void setFragment(String fragment) {
+        this.fragment = fragment;
+    }
+
+    /**
+     * @param doctype
+     *            the new value for doctype
+     */
+    public void setDoctype(String doctype) {
+        this.doctype = doctype;
+    }
+
+    /**
+     * @param charset
+     *            the new value for charset
+     */
+    public void setCharset(String charset) {
+        this.charset = charset;
+    }
+
+    /**
+     * @param debug
+     *            the new value for debug
+     */
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
+
+    @Override
+    public void execute() throws BuildException {
+        int notNullSource = 0;
+        if (uri != null) {
+            notNullSource++;
+        }
+        if (fragment != null) {
+            notNullSource++;
+        }
+        if (uploaded_file != null) {
+            notNullSource++;
+        }
+        if (notNullSource == 0) {
+            throw new BuildException("at least one of 'uri', 'fragment' or 'file' must be given!");
+        }
+        if (notNullSource > 1) {
+            throw new BuildException("Only one of 'uri', 'fragment' or 'file' can be given!");
+        }
+        List<URL> urlsToCheck = new ArrayList<URL>();
+        urlsToCheck.add(uri);
+        HashSet<String> checkedURIs = new HashSet<String>();
+        while (!urlsToCheck.isEmpty()) {
+            URL url = urlsToCheck.remove(0);
+            if (url != null) {
+                String uriString = url.toString();
+                if (checkedURIs.contains(uriString)) {
+                    continue;
+                }
+                checkedURIs.add(uriString);
+            }
+            if (checkURI(url)) {
+                if (recurse) {
+                    Set<URL> recurseInto = recurseInto(url);
+                    for (URL newUrl : recurseInto) {
+                        String string = newUrl.toString();
+                        if (checkedURIs.contains(string)) {
+                            continue;
+                        }
+                        for (Pattern pattern : ignorePatternList) {
+                            if (pattern.matcher(string).matches()) {
+                                continue;
+                            }
+                        }
+                        urlsToCheck.add(newUrl);
+                    }
+                }
+            }
+        }
+    }
+
+    protected boolean checkURI(final URL uriToCheck) throws BuildException {
+        try {
+            InputStream connection = buildConnection(uriToCheck);
+            Unmarshaller unmarshaller = JAXBContext.newInstance(ObjectFactory.class).createUnmarshaller();
+            Object object = getObject(unmarshaller.unmarshal(connection));
+            if (W3_ORG_VALIDATOR.equals(validator)) {
+                //The W3C recommends to at least wait one second between automatic requests to their public service...
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    //We don't care then...
+                    return false;
+                }
+            }
+            if (object instanceof Envelope) {
+                Envelope envelope = (Envelope) object;
+                for (Object bodyObject : envelope.getBody().getAny()) {
+                    bodyObject = getObject(bodyObject);
+                    if (bodyObject instanceof MarkupValidationResponse) {
+                        MarkupValidationResponse markupvalidationresponse = (MarkupValidationResponse) bodyObject;
+                        handleResponse(markupvalidationresponse);
+                        return true;
+                    } else {
+                        log("URL " + uriToCheck + " is ignored, it seem not to specify a valid document (e.g. link to binary file)", Project.MSG_DEBUG);
+                        continue;
+                    }
+                }
+                return false;
+            }
+            throw new BuildException("Invalid server response for URI: " + uriToCheck + " (was: " + object + ")");
+        } catch (MalformedURLException e) {
+            throw new BuildException("Bad URL for validation server", e);
+        } catch (JAXBException e) {
+            throw new BuildException("XML parser setup problem", e);
+        } catch (IOException e) {
+            throw new BuildException("Problem while communicating with server", e);
+        }
+    }
+
+    protected InputStream buildConnection(final URL uriToCheck) throws MalformedURLException, IOException, ProtocolException, UnsupportedEncodingException,
+            BuildException {
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new NameValuePair("output", VALIDATOR_FORMAT_OUTPUT));
+        if (uriToCheck != null) {
+            params.add(new NameValuePair("uri", uriToCheck.toString()));
+        } else {
+            if (fragment != null) {
+                params.add(new NameValuePair("fragment", fragment));
+            }
+        }
+        if (debug) {
+            params.add(new NameValuePair("debug", "1"));
+        }
+        if (charset != null) {
+            params.add(new NameValuePair("charset", charset));
+        }
+        if (doctype != null) {
+            params.add(new NameValuePair("doctype", doctype));
+        }
+        HttpClient httpClient = new HttpClient();
+        HttpMethodBase method;
+        if (uriToCheck != null) {
+            GetMethod getMethod = new GetMethod(validator);
+            getMethod.setQueryString(params.toArray(new NameValuePair[0]));
+            method = getMethod;
+        } else {
+            PostMethod postMethod = new PostMethod(validator);
+            if (fragment != null) {
+                postMethod.addParameters(params.toArray(new NameValuePair[0]));
+            } else {
+                postMethod.setRequestEntity(createFileUpload(params, postMethod.getParams()));
+            }
+            method = postMethod;
+        }
+        int result = httpClient.executeMethod(method);
+        if (result == HttpStatus.SC_OK) {
+            return method.getResponseBodyAsStream();
+        } else {
+            throw new BuildException("Server returned " + result + " " + method.getStatusText());
+        }
+
+    }
+
+    /**
+     * @param params
+     * @return
+     * @throws FileNotFoundException
+     */
+    private RequestEntity createFileUpload(List<NameValuePair> params, HttpMethodParams methodParams) throws FileNotFoundException {
+        List<Part> parts = new ArrayList<Part>();
+        for (NameValuePair nameValuePair : params) {
+            parts.add(new StringPart(nameValuePair.getName(), nameValuePair.getValue()));
+        }
+        FilePart fp = new FilePart("uploaded_file", uploaded_file);
+        fp.setContentType(URLConnection.guessContentTypeFromName(uploaded_file.getName()));
+        if (charset != null) {
+            fp.setCharSet(charset);
+        }
+        parts.add(fp);
+        return new MultipartRequestEntity(parts.toArray(new Part[0]), methodParams);
+    }
+
+    protected Object getObject(Object object) {
+        if (object instanceof JAXBElement<?>) {
+            object = ((JAXBElement<?>) object).getValue();
+        }
+        return object;
+    }
+
+    /**
+     * @param uriToRecurse
+     * @return
+     */
+    private Set<URL> recurseInto(final URL uriToRecurse) throws BuildException {
+        final Set<URL> urlsFound = new HashSet<URL>();
+        XMLReader reader = new Parser();
+        reader.setContentHandler(new DefaultHandler() {
+            @Override
+            public void startElement(String nsuri, String localName, String qName, Attributes attributes) throws SAXException {
+                if ("a".equalsIgnoreCase(qName)) {
+                    String value = attributes.getValue("href");
+                    if (value != null) {
+                        try {
+                            URL url = new URL(uriToRecurse, value);
+                            if (url.getHost().equalsIgnoreCase(uriToRecurse.getHost())) {
+                                urlsFound.add(url);
+                            }
+                        } catch (MalformedURLException e) {
+                            log("can't parse URL for href = " + value + ", it will be ignored!", Project.MSG_ERR);
+                        }
+                    }
+                }
+            }
+        });
+        // Parsen wird gestartet
+        try {
+            reader.parse(new InputSource(uriToRecurse.openStream()));
+            return urlsFound;
+        } catch (IOException e) {
+            throw new BuildException("error while accessing data at " + uriToRecurse, e);
+        } catch (SAXException e) {
+            throw new BuildException("error while parsing data at " + uriToRecurse, e);
+        }
+    }
+
+    /**
+     * @param markupvalidationresponse
+     */
+    private void handleResponse(MarkupValidationResponse response) {
+        log("URI:        " + response.getUri());
+        log("Doctype:    " + response.getDoctype());
+        log("Charset:    " + response.getCharset());
+        log("is valid:   " + response.isValidity());
+        List<Debug> debugList = response.getDebug();
+        for (Debug debug : debugList) {
+            log(String.format(debugPattern, response.getUri(), debug.getName(), debug.getValue()), Project.MSG_WARN);
+        }
+        ValidationErrors errors = response.getErrors();
+        if (errors != null) {
+            for (Error error : errors.getErrorlist().getError()) {
+                logMessage(errorPattern, response, error, Project.MSG_ERR);
+            }
+        }
+        ValidationWarnings warnings = response.getWarnings();
+        if (warnings != null) {
+            for (Warning warning : warnings.getWarninglist().getWarning()) {
+                logMessage(warningPattern, response, warning, Project.MSG_WARN);
+            }
+        }
+        if (!response.isValidity() && fail) {
+            throw new BuildException("Document at " + response.getUri() + " is invalid (" + response.getErrors().getErrorcount() + " errors)");
+        }
+    }
+
+    private void logMessage(String errorPattern, MarkupValidationResponse response, Culprit culprit, int level) {
+        log(String.format(errorPattern, culprit.getLine(), culprit.getCol(), culprit.getMessage(), culprit.getMessageid(), culprit.getSource(), culprit.getExplanation(), response.getUri()), level);
+    }
+
+}
